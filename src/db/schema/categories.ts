35c05c02c2ede4338e5cc1478c33c8c7fe607a8c
Milad Alizadeh <milaadd@gmail.com>
2023-08-02 17:20:59 +0100

    update recipes and categories schema

--- src/db/schema/categories.ts
+++ src/db/schema/categories.ts
@@ -1,6 +1,6 @@
 import { uuid, text, pgTable } from 'drizzle-orm/pg-core'
 
 export const categories = pgTable('categories', {
-  id: uuid('id').primaryKey(),
+  id: uuid('id').defaultRandom().primaryKey(),
   name: text('name'),
 })
--- src/db/schema/recipes.ts
+++ src/db/schema/recipes.ts
@@ -2,8 +2,8 @@ import { uuid, text, pgTable } from 'drizzle-orm/pg-core'
 import { categories } from './categories'
 
 export const recipes = pgTable('recipes', {
-  id: uuid('id').primaryKey(),
+  id: uuid('id').defaultRandom().primaryKey(),
   name: text('name'),
-  categoryId: text('category_id').references(() => categories.id),
+  categoryId: uuid('category_id').references(() => categories.id),
   imageUrl: text('image_url'),
 })
