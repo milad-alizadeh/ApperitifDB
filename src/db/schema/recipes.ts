9a4f6767383ecea36a562252bd18169c37ef8e07
Milad Alizadeh <milaadd@gmail.com>
2023-08-02 23:27:01 +0100

    generate initial migrations

--- src/db/schema/recipes.ts
+++ src/db/schema/recipes.ts
@@ -1,11 +1,9 @@
 import { uuid, text, pgTable } from 'drizzle-orm/pg-core'
-import { categories } from './categories'
 import { createdAt, updatedAt } from '../helpers'
 
 export const recipes = pgTable('recipes', {
   id: uuid('id').defaultRandom().primaryKey(),
   name: text('name'),
-  categoryId: uuid('category_id').references(() => categories.id),
   imageUrl: text('image_url'),
   createdAt,
   updatedAt,
