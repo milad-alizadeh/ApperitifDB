38bd8750178d2b508c33b7667fb18972ef9c327e
Milad Alizadeh <milaadd@gmail.com>
2023-08-02 22:44:27 +0100

    add new fields to ingredients

--- src/db/schema/ingredients.ts
+++ src/db/schema/ingredients.ts
@@ -4,6 +4,7 @@ import { createdAt, updatedAt } from '../helpers'
 export const ingredients = pgTable('ingredients', {
   id: uuid('id').defaultRandom().primaryKey(),
   name: text('name'),
+  unit: text('unit'),
   createdAt,
   updatedAt,
 })
--- src/db/schema/recipes_ingredients.ts
+++ src/db/schema/recipes_ingredients.ts
@@ -1,4 +1,4 @@
-import { uuid, pgTable } from 'drizzle-orm/pg-core'
+import { uuid, pgTable, integer } from 'drizzle-orm/pg-core'
 import { createdAt, updatedAt } from '../helpers'
 import { recipes } from './recipes'
 import { ingredients } from './ingredients'
@@ -6,6 +6,7 @@ import { ingredients } from './ingredients'
 export const recipesIngredients = pgTable('recipes_ingredients', {
   recipeId: uuid('recipe_id').references(() => recipes.id),
   ingredientId: uuid('ingredient_id').references(() => ingredients.id),
+  quantity: integer('text'),
   createdAt,
   updatedAt,
 })
