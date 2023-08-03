1c943177e8c4d96f47ec645410598b658f85bfbb
Milad Alizadeh <milaadd@gmail.com>
2023-08-03 02:48:57 +0100

    add primary key to pivot tables

--- src/db/schema/recipes_categories.ts
+++ src/db/schema/recipes_categories.ts
@@ -4,6 +4,7 @@ import { recipes } from './recipes'
 import { categories } from './categories'
 
 export const recipesCategories = pgTable('recipes_categories', {
+  id: uuid('id').defaultRandom().primaryKey(),
   recipeId: uuid('recipe_id').references(() => recipes.id),
   categoryId: uuid('category_id').references(() => categories.id),
   createdAt,
--- src/db/schema/recipes_ingredients.ts
+++ src/db/schema/recipes_ingredients.ts
@@ -4,6 +4,7 @@ import { recipes } from './recipes'
 import { ingredients } from './ingredients'
 
 export const recipesIngredients = pgTable('recipes_ingredients', {
+  id: uuid('id').defaultRandom().primaryKey(),
   recipeId: uuid('recipe_id').references(() => recipes.id),
   ingredientId: uuid('ingredient_id').references(() => ingredients.id),
   quantity: integer('quantity'),
