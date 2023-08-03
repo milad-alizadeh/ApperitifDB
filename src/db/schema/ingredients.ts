2039715d89722499b3e7104d9f05405a51a6cabf
Milad Alizadeh <milaadd@gmail.com>
2023-08-03 01:25:11 +0100

    add a weight column to ingredient

--- src/db/schema/ingredients.ts
+++ src/db/schema/ingredients.ts
@@ -1,10 +1,11 @@
-import { uuid, text, pgTable } from 'drizzle-orm/pg-core'
+import { uuid, text, pgTable, integer } from 'drizzle-orm/pg-core'
 import { createdAt, updatedAt } from '../helpers'
 
 export const ingredients = pgTable('ingredients', {
   id: uuid('id').defaultRandom().primaryKey(),
   name: text('name'),
   unit: text('unit'),
+  weight: integer('weight'),
   createdAt,
   updatedAt,
 })
