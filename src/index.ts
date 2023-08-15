139317ad3e0e423b811eff46fda09638eb46e9dd
Milad Alizadeh <milaadd@gmail.com>
2023-08-15 12:50:53 +0100

    add a basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-export const sayHello = (name: string): void => {
-  console.log(`Hello, ${name}!`)
-}
+import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import { schema } from './graphql/schema'
 
-sayHello('world')
+const app = express()
+const port = 3000
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  }),
+)
+
+app.listen(port, () => {
+  console.log(`Express listening at http://localhost:${port}`)
+})
