fcd9956ab7ae7e33d827c3137c25b34dcca9c455
Milad Alizadeh <milaadd@gmail.com>
2023-08-22 00:46:42 +0100

    add more data to the seed file

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -36,6 +36,16 @@ VALUES
     'f6a16ff7-4a31-11eb-be7b-8344edc8f36b',
     'Whiskey Sours',
     'https://source.unsplash.com/random/500×500/?cocktails'
+  ),
+  (
+    '5c6914d1-d898-40d4-abb7-44b862967245',
+    'Mojito',
+    'https://source.unsplash.com/random/500x500/?mojito'
+  ),
+  (
+    'c77587f4-b5ce-4987-9562-636202b52f30',
+    'Margarita',
+    'https://source.unsplash.com/random/500x500/?margarita'
   );
 
 INSERT INTO
@@ -63,6 +73,42 @@ VALUES
     'f6a16ff7-4a31-11eb-be7b-8344edc8f36b',
     3,
     'Strain into a rocks glass over ice and garnish with a lemon zest.'
+  ),
+  (
+    '4efa4080-d466-4933-9810-5eb5e8f6f5d0',
+    '5c6914d1-d898-40d4-abb7-44b862967245',
+    1,
+    'In a glass muddle the lime, sugar and mint leaves.'
+  ),
+  (
+    '2b7c3d76-a10e-4f4c-a4c3-c4dc4cb11490',
+    '5c6914d1-d898-40d4-abb7-44b862967245',
+    2,
+    'Add rum and top with soda. Stir gently.'
+  ),
+  (
+    '1f3d08d6-5970-4621-81d9-36518d45713e',
+    '5c6914d1-d898-40d4-abb7-44b862967245',
+    3,
+    'Fill glass with ice and garnish with mint.'
+  ),
+  (
+    '6cbc7b3a-0b15-4b25-b8ba-4762e933efe3',
+    'c77587f4-b5ce-4987-9562-636202b52f30',
+    1,
+    'Rim half the glass with salt.'
+  ),
+  (
+    '8569551c-a8c3-47b1-9cc7-79dc3cb41adc',
+    'c77587f4-b5ce-4987-9562-636202b52f30',
+    2,
+    'Fill a cocktail shaker with ice. Add tequila, orange liqueur and lime juice. Shake well.'
+  ),
+  (
+    '0c747927-8a8f-4b09-86d8-f65b8e61842a',
+    'c77587f4-b5ce-4987-9562-636202b52f30',
+    3,
+    'Strain into the prepared glass filled with ice. Garnish with lime wedge.'
   );
 
 INSERT INTO
@@ -121,6 +167,42 @@ VALUES
     'Simple Syrup',
     5,
     'https://source.unsplash.com/random/500×500/?sugar'
+  ),
+  (
+    'c1a8a30d-b6e6-47e0-9d26-84a3b5d533d3',
+    'Lime',
+    6,
+    'https://source.unsplash.com/random/500x500/?lime'
+  ),
+  (
+    'a2a2e23d-c095-420c-b9b0-e1493a7d35d3',
+    'Mint',
+    7,
+    'https://source.unsplash.com/random/500x500/?mint'
+  ),
+  (
+    '53655728-9114-4db4-af07-e28da6e24edd',
+    'Light Rum',
+    8,
+    'https://source.unsplash.com/random/500x500/?rum'
+  ),
+  (
+    'c9bf47cf-063a-4188-8ae9-2e98f5150e49',
+    'Club Soda',
+    9,
+    'https://source.unsplash.com/random/500x500/?soda'
+  ),
+  (
+    'edf391a5-f3b0-4325-9318-c0d64d78d0d5',
+    'Tequila',
+    10,
+    'https://source.unsplash.com/random/500x500/?tequila'
+  ),
+  (
+    '1f8d8a96-8e80-4c44-b8f2-8044be2d3b09',
+    'Orange Liqueur',
+    11,
+    'https://source.unsplash.com/random/500x500/?orange-liqueur'
   );
 
 INSERT INTO
@@ -135,6 +217,16 @@ VALUES
     'ad3766f1-e8d2-43d6-a093-a0734d409b6f',
     'f6a16ff7-4a31-11eb-be7b-8344edc8f36b',
     'c2b8c518-54ab-4475-b169-891afe039d17'
+  ),
+  (
+    '9e9c2eab-8622-4b1b-a545-4a4671000bb3',
+    '5c6914d1-d898-40d4-abb7-44b862967245',
+    'c2b8c518-54ab-4475-b169-891afe039d17'
+  ),
+  (
+    'c081c6b3-2600-4b69-bd09-9ebeaa3c8bd6',
+    'c77587f4-b5ce-4987-9562-636202b52f30',
+    '4acb9163-4149-47e2-b98a-2e84b74c711e'
   );
 
 INSERT INTO
@@ -180,4 +272,53 @@ VALUES
     '82fb793b-0401-402d-a103-6eacde0ac434',
     '679f2e43-f302-4f99-928e-209086a41206',
     22.5
+  ),
+  (
+    '8b7ae817-7c0d-47e5-a24e-5c805c44968c',
+    '5c6914d1-d898-40d4-abb7-44b862967245',
+    'c1a8a30d-b6e6-47e0-9d26-84a3b5d533d3',
+    '4c403c4c-7eaf-4579-a50c-af0c9a5a05a9',
+    1
+  ),
+  (
+    'ea2504a7-1e36-418d-bee7-b326fae8e236',
+    '5c6914d1-d898-40d4-abb7-44b862967245',
+    'a2a2e23d-c095-420c-b9b0-e1493a7d35d3',
+    '5c2f3854-4eb8-482a-b7ac-47842139da2c',
+    3
+  ),
+  (
+    '6f685324-2096-4ccd-a1c3-ba78e1076f6b',
+    '5c6914d1-d898-40d4-abb7-44b862967245',
+    '53655728-9114-4db4-af07-e28da6e24edd',
+    '3b0d354b-c091-449a-844b-0d64e00e1a24',
+    1.5
+  ),
+  (
+    '91c9ddd1-92f4-4ac2-9567-3d29ae2a61f4',
+    '5c6914d1-d898-40d4-abb7-44b862967245',
+    'c9bf47cf-063a-4188-8ae9-2e98f5150e49',
+    '4c403c4c-7eaf-4579-a50c-af0c9a5a05a9',
+    120
+  ),
+  (
+    '856ce10a-5e24-4af0-b9dc-0b58d96ad9be',
+    'c77587f4-b5ce-4987-9562-636202b52f30',
+    'edf391a5-f3b0-4325-9318-c0d64d78d0d5',
+    '3b0d354b-c091-449a-844b-0d64e00e1a24',
+    1.5
+  ),
+  (
+    '328867ab-25e2-42bb-9663-1d2df6baf050',
+    'c77587f4-b5ce-4987-9562-636202b52f30',
+    '1f8d8a96-8e80-4c44-b8f2-8044be2d3b09',
+    '4c403c4c-7eaf-4579-a50c-af0c9a5a05a9',
+    30
+  ),
+  (
+    '2e215a72-ad9b-4888-aea0-57e7a867217c',
+    'c77587f4-b5ce-4987-9562-636202b52f30',
+    'c1a8a30d-b6e6-47e0-9d26-84a3b5d533d3',
+    '4c403c4c-7eaf-4579-a50c-af0c9a5a05a9',
+    30
   );
