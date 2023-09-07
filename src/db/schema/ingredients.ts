bbe259afca35229897be8c91ba39afd3a9dd5c05
Milad Alizadeh <milaadd@gmail.com>
2023-09-07 19:38:17 +0100

    remove imageUrl and weight from ingredients

--- src/db/schema/ingredients.ts
+++ src/db/schema/ingredients.ts
@@ -1,11 +1,9 @@
-import { uuid, text, pgTable, integer } from 'drizzle-orm/pg-core'
+import { uuid, text, pgTable } from 'drizzle-orm/pg-core'
 import { createdAt, updatedAt } from '../helpers'
 
 export const ingredients = pgTable('ingredients', {
   id: uuid('id').defaultRandom().primaryKey(),
   name: text('name'),
-  weight: integer('weight'),
-  imageUrl: text('image_url'),
   description: text('description'),
   createdAt,
   updatedAt,
