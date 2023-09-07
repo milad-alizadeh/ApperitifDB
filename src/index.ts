f4ffe776160193a886f24e2a5037a30c6960306e
Milad Alizadeh <milaadd@gmail.com>
2023-09-07 18:43:37 +0100

    add see categories, ingredients and recipes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-export const sayHello = (name: string): void => {
-  console.log(`Hello, ${name}!`)
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import { recipes } from './db/schema/recipes'
+import { faker } from '@faker-js/faker'
+
+const client = postgres(
+  'postgresql://postgres:postgres@localhost:54322/postgres',
+)
+const db = drizzle(client)
+
+const insertRecipes = async (): Promise<void> => {
+  const image = faker
+
+  const allRecipes = await db.select().from(recipes)
+  console.log(allRecipes, image)
 }
 
-sayHello('world')
+insertRecipes()
