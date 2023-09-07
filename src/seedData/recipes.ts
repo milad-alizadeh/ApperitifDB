846bf919a0e342186e830764a466a250d58a6ebc
Milad Alizadeh <milaadd@gmail.com>
2023-09-07 19:07:37 +0100

    update recipe seeds

--- src/seedData/recipes.ts
+++ src/seedData/recipes.ts
@@ -1,55 +1,330 @@
 export default [
-  'Amaretto Sour',
-  'Apple Pie',
-  'Aviation',
-  'Blackberry Margarita',
-  'Boulevardier',
-  'Cosmopolitan',
-  'Cosmopolitan (Low-Cal)',
-  'Espresso Martini',
-  'Gimlet',
-  'Gin Martini',
-  'Ginger Peach Iced Tea',
-  'Hemingway Daiquiri',
-  'Holiday Spiced Old Fashioned',
-  'Jalapeño Lime Shrub',
-  'Kamikaze',
-  'Lemon Drop',
-  'Long Island Iced Tea',
-  'Low-Cal Cosmopolitan',
-  'Low-Cal Margarita',
-  'Low-Cal Whiskey Sour',
-  'Mai Tai',
-  'Mango Margarita',
-  'Manhattan',
-  'Margarita',
-  'Margarita - Mango',
-  'Margarita - Mistletoe',
-  'Margarita - Spicy',
-  'Margarita (Low-Cal)',
-  'Matcha Martini',
-  'Mint Julep',
-  'Mistletoe Margarita',
-  'Negroni',
-  'Northside',
-  'Old Fashioned',
-  'Old-Fashioned',
-  'Old-Fashioned - Holiday Spiced',
-  'Paper Plane',
-  'Passion Fruit Margarita',
-  'Rhubarb Spice Shrub',
-  'Rum Breeze',
-  'Rum Runner',
-  'Sazerac',
-  'Screwdriver',
-  'Sex on the Beach',
-  'Shrub - Jalapeño Lime',
-  'Shrub - Rhubarb Spice',
-  'Spiced Coffee',
-  'Spicy Margarita',
-  'Uptown Rocks',
-  'Whiskey Smash',
-  'Whiskey Sour',
-  'Whiskey Sour  (Low-Cal)',
-  "Witch's Heart",
+  {
+    name: 'Amaretto Sour',
+    description:
+      'The Amaretto Sour offers a delightful blend of almond-flavored amaretto with the tang of lemon juice. It gained popularity in the 1970s as a smooth cocktail option.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Amaretto+Sour',
+  },
+  {
+    name: 'Apple Pie',
+    description:
+      'Apple Pie cocktail is a delightful autumn treat. It combines the essence of apple cider with a hint of cinnamon, reminiscent of the classic dessert.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Apple+Pie',
+  },
+  {
+    name: 'Aviation',
+    description:
+      'Originating in the early 20th century, Aviation is a classic cocktail that blends gin, maraschino liqueur, crème de violette, and lemon juice, producing a sky-blue drink.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Aviation',
+  },
+  {
+    name: 'Blackberry Margarita',
+    description:
+      'The Blackberry Margarita infuses the classic margarita with rich blackberry flavors, offering a fruity twist on a beloved classic. It became trendy in the late 90s for its rich hue and refreshing taste.',
+    imageUrl:
+      'https://source.unsplash.com/random/500x500/?Blackberry+Margarita',
+  },
+  {
+    name: 'Boulevardier',
+    description:
+      "The Boulevardier is essentially a whiskey Negroni. With a history dating back to the 1920s Paris, it's a sophisticated blend of whiskey, Campari, and sweet vermouth.",
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Boulevardier',
+  },
+  {
+    name: 'Cosmopolitan',
+    description:
+      'Popularized in the 90s by the TV show "Sex and the City", the Cosmopolitan is a classy blend of vodka, cranberry juice, triple sec, and lime juice. It’s a favorite among many for its bright pink hue and crisp taste.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Cosmopolitan',
+  },
+  {
+    name: 'Cosmopolitan (Low-Cal)',
+    description:
+      'A healthier twist on the classic, the Low-Cal Cosmopolitan maintains the signature flavor profile but with fewer calories, making it a favorite among health-conscious drinkers.',
+    imageUrl:
+      'https://source.unsplash.com/random/500x500/?Low-Cal+Cosmopolitan',
+  },
+  {
+    name: 'Espresso Martini',
+    description:
+      'The Espresso Martini is a coffee lover’s dream. Born in London in the 80s, this cocktail combines vodka, coffee liqueur, and freshly brewed espresso. It’s a delightful post-dinner or pick-me-up cocktail.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Espresso+Martini',
+  },
+  {
+    name: 'Gimlet',
+    description:
+      'The Gimlet is a straightforward and sharp cocktail, often made with gin and lime juice. It has naval origins, historically consumed by sailors to prevent scurvy.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Gimlet',
+  },
+  {
+    name: 'Gin Martini',
+    description:
+      "The Gin Martini is a hallmark of cocktail elegance. Often stirred with vermouth and garnished with an olive or lemon twist, it's been a symbol of sophistication since the early 20th century.",
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Gin+Martini',
+  },
+  {
+    name: 'Ginger Peach Iced Tea',
+    description:
+      "A refreshing blend of bold tea, juicy peach, and spicy ginger, the Ginger Peach Iced Tea is the perfect beverage for a hot summer day. Its origins are rooted in Southern traditions, blending the area's love for iced tea and fresh fruits.",
+    imageUrl:
+      'https://source.unsplash.com/random/500x500/?Ginger+Peach+Iced+Tea',
+  },
+  {
+    name: 'Hemingway Daiquiri',
+    description:
+      'Named after the famous writer Ernest Hemingway, this daiquiri variation boasts grapefruit juice and maraschino liqueur. Hemingway was known to frequent a bar in Havana where he enjoyed this tangy concoction.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Hemingway+Daiquiri',
+  },
+  {
+    name: 'Holiday Spiced Old Fashioned',
+    description:
+      'The Holiday Spiced Old Fashioned infuses the time-honored cocktail with seasonal flavors like cinnamon and nutmeg. It’s a warming treat that’s perfect for festive celebrations.',
+    imageUrl:
+      'https://source.unsplash.com/random/500x500/?Holiday+Spiced+Old+Fashioned',
+  },
+  {
+    name: 'Jalapeño Lime Shrub',
+    description:
+      'A delightful mix of spicy and tangy, the Jalapeño Lime Shrub is a vibrant drink that combines the heat of jalapeños with the zesty freshness of lime. It’s a modern concoction favored by those who enjoy a kick in their sip.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Jalapeño+Lime+Shrub',
+  },
+  {
+    name: 'Kamikaze',
+    description:
+      'The Kamikaze is a potent blend of vodka, triple sec, and lime juice. Its name, meaning "divine wind" in Japanese, aptly captures the drink\'s boldness and is a nod to its post-World War II American origins.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Kamikaze',
+  },
+  {
+    name: 'Lemon Drop',
+    description:
+      "Sweet and sour, the Lemon Drop cocktail is a vodka-based treat that's reminiscent of the classic candy. Garnished with a sugar rim, it’s a delightful blend of tart lemon juice and sweet liqueur.",
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Lemon+Drop',
+  },
+  {
+    name: 'Long Island Iced Tea',
+    description:
+      'Despite its name, the Long Island Iced Tea contains no actual tea. This powerful cocktail blends together vodka, tequila, rum, gin, triple sec, lemon juice, and cola, creating a drink that’s both potent and surprisingly smooth.',
+    imageUrl:
+      'https://source.unsplash.com/random/500x500/?Long+Island+Iced+Tea',
+  },
+  {
+    name: 'Low-Cal Cosmopolitan',
+    description:
+      'The Low-Cal Cosmopolitan offers the beloved taste of the classic Cosmo but with fewer calories. This cocktail became popular among those who love the vibrant flavors but seek a lighter option.',
+    imageUrl:
+      'https://source.unsplash.com/random/500x500/?Low-Cal+Cosmopolitan',
+  },
+  {
+    name: 'Low-Cal Margarita',
+    description:
+      'Offering a guilt-free twist on a timeless favorite, the Low-Cal Margarita captures the essence of the classic drink, blending tequila with lime and other mixers, all while keeping the calorie count in check.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Low-Cal+Margarita',
+  },
+  {
+    name: 'Low-Cal Whiskey Sour',
+    description:
+      'The Low-Cal Whiskey Sour offers whiskey enthusiasts a chance to savor their preferred spirit with a tangy twist, without the usual calorie content. Its balanced blend of flavors ensures you don’t compromise on taste.',
+    imageUrl:
+      'https://source.unsplash.com/random/500x500/?Low-Cal+Whiskey+Sour',
+  },
+  {
+    name: 'Mai Tai',
+    description:
+      'The Mai Tai is a tropical delight that transports you straight to the beaches of Hawaii. With a blend of rum, lime juice, and other tropical flavors, its name aptly translates to "good" from Tahitian.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Mai+Tai',
+  },
+  {
+    name: 'Mango Margarita',
+    description:
+      'The Mango Margarita offers a fruity twist on the classic margarita. The rich, tropical flavor of mango combined with the kick of tequila makes it a favorite choice for summer gatherings.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Mango+Margarita',
+  },
+  {
+    name: 'Manhattan',
+    description:
+      'A classic that originated in the late 19th century, the Manhattan elegantly combines whiskey, sweet vermouth, and bitters. This cocktail, named after the New York City borough, is often garnished with a cherry.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Manhattan',
+  },
+  {
+    name: 'Margarita',
+    description:
+      'The Margarita, with its perfect balance of tequila, lime juice, and triple sec, is a beloved classic around the world. Its origins are debated, but its appeal is undeniable, often served with a salted rim.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Margarita',
+  },
+  {
+    name: 'Margarita - Mango',
+    description:
+      'Adding a tropical twist to the traditional, the Mango Margarita combines the classic ingredients with ripe mango, resulting in a refreshing and sweet variation of the original.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Mango+Margarita',
+  },
+  {
+    name: 'Margarita - Mistletoe',
+    description:
+      'A festive twist on a classic, the Mistletoe Margarita combines the traditional flavors with hints of cranberry and pomegranate. Perfect for holiday celebrations, this cocktail adds a colorful and merry touch to any gathering.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Mistletoe+Margarita',
+  },
+  {
+    name: 'Margarita - Spicy',
+    description:
+      'For those who enjoy a kick with their cocktail, the Spicy Margarita blends classic margarita ingredients with a touch of heat, often coming from jalapeños or chili-infused tequila.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Spicy+Margarita',
+  },
+  {
+    name: 'Margarita (Low-Cal)',
+    description:
+      "For the calorie-conscious tequila lover, the Low-Cal Margarita delivers all the tangy satisfaction of the original without the guilt. A clever mix of ingredients ensures flavor isn't compromised.",
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Low-Cal+Margarita',
+  },
+  {
+    name: 'Matcha Martini',
+    description:
+      'A fusion of East meets West, the Matcha Martini blends the earthy flavors of matcha green tea with the crispness of a classic martini. This contemporary drink is both energizing and sophisticated.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Matcha+Martini',
+  },
+  {
+    name: 'Mint Julep',
+    description:
+      "Deeply rooted in the American South and synonymous with the Kentucky Derby, the Mint Julep is a refreshing blend of bourbon, fresh mint, and sugar. Served over crushed ice, it's a cooling drink perfect for warm days.",
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Mint+Julep',
+  },
+  {
+    name: 'Mistletoe Margarita',
+    description:
+      'Reimagining the traditional Margarita with festive flair, the Mistletoe Margarita blends tequila with seasonal berries and citrus. It’s the ideal cocktail for toasting to the holidays.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Mistletoe+Margarita',
+  },
+  {
+    name: 'Negroni',
+    description:
+      'The Negroni is a classic Italian cocktail, known for its bright red color and balanced taste. Comprising equal parts of gin, Campari, and sweet vermouth, it’s both bitter and sweet with a rich history.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Negroni',
+  },
+  {
+    name: 'Northside',
+    description:
+      "The Northside is a refreshing gin cocktail that combines the botanical spirit with fresh mint and lime. It's a crisp, tangy drink perfect for any occasion or season.",
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Northside',
+  },
+  {
+    name: 'Old Fashioned',
+    description:
+      'The Old Fashioned is a cocktail made by muddling sugar with bitters, adding alcohol, and a twist of citrus rind. It is traditionally served in a short, round, tumbler-like glass, and is one of the classics of the cocktail world.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Old+Fashioned',
+  },
+  {
+    name: 'Old-Fashioned',
+    description:
+      'Dating back to the 1800s, the Old-Fashioned is one of the oldest known cocktails. Combining whiskey, sugar, and bitters, it’s a drink that emphasizes the whiskey’s flavor profile.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Old-Fashioned',
+  },
+  {
+    name: 'Old-Fashioned - Holiday Spiced',
+    description:
+      'A festive twist on the classic, the Holiday Spiced Old-Fashioned infuses traditional ingredients with spices like cinnamon and clove, perfect for sipping by a roaring fire during the winter months.',
+    imageUrl:
+      'https://source.unsplash.com/random/500x500/?Holiday+Spiced+Old-Fashioned',
+  },
+  {
+    name: 'Paper Plane',
+    description:
+      'The Paper Plane is a modern classic, combining bourbon, Aperol, Amaro Nonino, and lemon juice. With its delightful balance of bitter and sweet, it’s a cocktail that takes flight on the palate.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Paper+Plane',
+  },
+  {
+    name: 'Passion Fruit Margarita',
+    description:
+      'An exotic take on the traditional margarita, the Passion Fruit Margarita combines the tartness of passion fruit with the robust flavors of tequila. This tropical drink is a true palate pleaser.',
+    imageUrl:
+      'https://source.unsplash.com/random/500x500/?Passion+Fruit+Margarita',
+  },
+  {
+    name: 'Rhubarb Spice Shrub',
+    description:
+      'The Rhubarb Spice Shrub blends the tangy flavors of rhubarb with warm spices, making it a unique and delightful drink. This vinegar-based syrup cocktail is both refreshing and aromatic.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Rhubarb+Spice+Shrub',
+  },
+  {
+    name: 'Rum Breeze',
+    description:
+      'The Rum Breeze is a tropical treat that combines rum with a medley of fruity flavors. This cocktail whisks you away to a beach setting with every sip, making it a vacation in a glass.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Rum+Breeze',
+  },
+  {
+    name: 'Rum Runner',
+    description:
+      "Born in the Florida Keys in the 1950s, the Rum Runner combines rum with a variety of fruit liqueurs and juices, resulting in a sweet and tropical concoction that's perfect for seaside sipping.",
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Rum+Runner',
+  },
+  {
+    name: 'Sazerac',
+    description:
+      'Hailing from New Orleans, the Sazerac is a blend of rye whiskey, absinthe, sugar, and Peychaud’s bitters. This sophisticated cocktail has a storied history and is known for its unique preparation method.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Sazerac',
+  },
+  {
+    name: 'Screwdriver',
+    description:
+      'The Screwdriver is a simple yet iconic cocktail made of vodka and orange juice. It gained popularity in the mid-20th century and is often served in a tall glass over ice.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Screwdriver',
+  },
+  {
+    name: 'Sex on the Beach',
+    description:
+      'A vibrant and fruity cocktail, Sex on the Beach combines vodka, peach schnapps, orange and cranberry juices. Its catchy name and delicious taste make it a popular choice at beach resorts and bars alike.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Sex+on+the+Beach',
+  },
+  {
+    name: 'Shrub - Jalapeño Lime',
+    description:
+      "A delightful blend of spicy and tangy, the Jalapeño Lime Shrub is a vinegar-based syrup cocktail that's both refreshing and invigorating. Perfect for those who like a little kick in their sip.",
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Jalapeño+Lime+Shrub',
+  },
+  {
+    name: 'Shrub - Rhubarb Spice',
+    description:
+      'Rhubarb Spice Shrub offers a tangy and spicy twist to the traditional shrub drink. The tartness of rhubarb combined with warm spices creates a unique and delightful taste experience.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Rhubarb+Spice+Shrub',
+  },
+  {
+    name: 'Spiced Coffee',
+    description:
+      "The perfect blend of warmth and energy, Spiced Coffee combines classic coffee flavors with aromatic spices like cinnamon and nutmeg. It's a comforting drink, ideal for sipping on chilly mornings.",
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Spiced+Coffee',
+  },
+  {
+    name: 'Spicy Margarita',
+    description:
+      'A fiery twist on a classic, the Spicy Margarita infuses traditional margarita ingredients with spicy elements, often jalapeños. The heat from the chili pairs wonderfully with the tang of lime and tequila.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Spicy+Margarita',
+  },
+  {
+    name: 'Uptown Rocks',
+    description:
+      'Uptown Rocks, with its sleek and smooth profile, is a modern cocktail that combines a variety of spirits and mixers. It’s known for its sophisticated flavor and presentation.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Uptown+Rocks',
+  },
+  {
+    name: 'Whiskey Smash',
+    description:
+      "A close relative of the Mint Julep, the Whiskey Smash combines whiskey, mint, sugar, and citrus. Muddled together, these ingredients create a refreshing and tangy cocktail that's perfect for sipping on a warm day.",
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Whiskey+Smash',
+  },
+  {
+    name: 'Whiskey Sour',
+    description:
+      "The Whiskey Sour is a classic cocktail that blends whiskey, lemon juice, and sugar to create a tart and refreshing drink. Often garnished with a cherry and/or lemon slice, it's a beloved choice for many.",
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Whiskey+Sour',
+  },
+  {
+    name: 'Whiskey Sour  (Low-Cal)',
+    description:
+      'For those mindful of their calorie intake but still wanting a flavorful cocktail, the Low-Cal Whiskey Sour is the answer. It maintains the tartness and flavor of the classic version, but with fewer calories.',
+    imageUrl:
+      'https://source.unsplash.com/random/500x500/?Low-Cal+Whiskey+Sour',
+  },
+  {
+    name: "Witch's Heart",
+    description:
+      'A bewitching concoction perfect for Halloween, the Witch’s Heart combines vibrant, shimmering liquors with fruit flavors. It’s not only delicious but also a visual spectacle, making it a party favorite.',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Witchs+Heart',
+  },
 ]
