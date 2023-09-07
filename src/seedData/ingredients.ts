b256b1790708cc0099d1867f2fa764eef5a7935c
Milad Alizadeh <milaadd@gmail.com>
2023-09-07 19:38:53 +0100

    add ingredients see data

--- src/seedData/ingredients.ts
+++ src/seedData/ingredients.ts
@@ -1,54 +1,247 @@
 export default [
-  'Vodka',
-  'Gin',
-  'Rum',
-  'Tequila',
-  'Whiskey',
-  'Bourbon',
-  'Brandy',
-  'Cognac',
-  'Champagne',
-  'Prosecco',
-  'Red Wine',
-  'White Wine',
-  'Rosé Wine',
-  'Campari',
-  'Aperol',
-  'Vermouth',
-  'Triple Sec',
-  'Cointreau',
-  'Grand Marnier',
-  'Blue Curaçao',
-  'Peach Schnapps',
-  'Midori',
-  'Baileys Irish Cream',
-  'Kahlúa',
-  'Frangelico',
-  'Amaretto',
-  'Chambord',
-  'Galliano',
-  'Chartreuse',
-  'Benedictine',
-  'Angostura Bitters',
-  'Peychaud’s Bitters',
-  'Orange Bitters',
-  'Grenadine',
-  'Simple Syrup',
-  'Honey Syrup',
-  'Agave Syrup',
-  'Lime Juice',
-  'Lemon Juice',
-  'Orange Juice',
-  'Cranberry Juice',
-  'Pineapple Juice',
-  'Grapefruit Juice',
-  'Coconut Cream',
-  'Heavy Cream',
-  'Egg White',
-  'Mint Leaves',
-  'Basil Leaves',
-  'Rosemary Sprig',
-  'Cucumber',
-  'Ginger',
-  'Jalapeño',
+  {
+    name: 'Amaretto',
+    description:
+      'Amaretto is an almond-flavored Italian liqueur. It is made from a base of apricot or almond pits, or sometimes both. While originally from Italy, it has become popular worldwide and is often used in a variety of cocktails and desserts.',
+  },
+  {
+    name: 'Bitters',
+    description:
+      'Bitters are highly concentrated alcoholic beverages flavored with botanicals. They add complexity to a range of cocktails and are a staple in classic recipes like the Old Fashioned and the Manhattan.',
+  },
+  {
+    name: 'Bourbon',
+    description:
+      'Bourbon is an American type of whiskey, barrel-aged and made primarily of corn. Known for its rich, sweet, full-bodied flavor, it serves as the base for many classic cocktails such as the Mint Julep and Whiskey Sour.',
+  },
+  {
+    name: 'Brandy',
+    description:
+      'Brandy is a spirit made from the distillation of wine or fermented fruit juice. Often enjoyed as an after-dinner drink, brandy is also commonly used in classic cocktails like the Sidecar.',
+  },
+  {
+    name: 'Cachaça',
+    description:
+      "Cachaça is a Brazilian spirit made from sugarcane juice. It’s the key ingredient in the Caipirinha, Brazil's national cocktail. The liquor has a robust, rustic flavor that pairs well with sweet and tart mixers.",
+  },
+  {
+    name: 'Campari',
+    description:
+      "Campari is an Italian aperitif famous for its bitter, complex flavor profile. It's often used in cocktails like the Negroni and the Americano to add a touch of bitterness and color.",
+  },
+  {
+    name: 'Champagne',
+    description:
+      'Champagne is a sparkling wine from the Champagne region of France. It is often used in celebratory drinks and cocktails like the Mimosa or the French 75.',
+  },
+  {
+    name: 'Cointreau',
+    description:
+      "Cointreau is a brand of French triple sec, an orange-flavored liqueur. It's commonly used in cocktails like the Margarita and Cosmopolitan to add sweetness and citrus notes.",
+  },
+  {
+    name: 'Gin',
+    description:
+      "Gin is a distilled alcoholic beverage that derives its predominant flavor from juniper berries. It's a versatile spirit, serving as the foundation for numerous cocktails, including the Martini and the Gin and Tonic.",
+  },
+  {
+    name: 'Grenadine',
+    description:
+      'Grenadine is a commonly used, non-alcoholic syrup made from pomegranate juice. It adds a splash of color and sweetness to various cocktails like the Tequila Sunrise and Sea Breeze.',
+  },
+  {
+    name: 'Kahlúa',
+    description:
+      'Kahlúa is a rich coffee liqueur originating from Mexico. It has a sweet, syrupy consistency and is often used in cocktails such as the White Russian and Espresso Martini.',
+  },
+  {
+    name: 'Lemon Juice',
+    description:
+      'Lemon juice is a commonly used citrus in cocktails, providing a fresh and tangy flavor. It is an integral ingredient in classics like the Whiskey Sour and Lemon Drop, balancing sweetness and adding brightness.',
+  },
+  {
+    name: 'Lime Juice',
+    description:
+      'Lime juice, another popular citrus in mixology, offers a zesty tang that livens up a multitude of cocktails. From the Gimlet to the Margarita, its sharpness cuts through sweeter ingredients and adds a refreshing kick.',
+  },
+  {
+    name: 'Mint Leaves',
+    description:
+      'Mint leaves are often muddled or used as a garnish in cocktails, contributing a cooling, aromatic freshness. Drinks like the Mojito and Mint Julep are defined by their minty flavor profile.',
+  },
+  {
+    name: 'Peach Schnapps',
+    description:
+      'Peach Schnapps is a sweet liqueur infused with the essence of peaches. It’s used in a variety of cocktails, adding a fruity dimension to drinks like the Sex on the Beach and Fuzzy Navel.',
+  },
+  {
+    name: 'Pineapple Juice',
+    description:
+      'Pineapple juice provides a tropical, sweet-tart element to cocktails. Frequently used in drinks like the Piña Colada and Rum Punch, it pairs especially well with rum and coconut flavors.',
+  },
+  {
+    name: 'Rum',
+    description:
+      'Rum is a distilled alcoholic beverage made from sugarcane byproducts, such as molasses, or directly from sugarcane juice. With varieties ranging from white to dark, it’s the spirit of choice in tropical cocktails like the Mai Tai and Daiquiri.',
+  },
+  {
+    name: 'Simple Syrup',
+    description:
+      'Simple syrup is a basic sugar-water solution used to sweeten cocktails. Its neutral flavor profile means it can be added to virtually any drink that requires a touch of sweetness, making it a bar staple.',
+  },
+  {
+    name: 'Tequila',
+    description:
+      'Tequila is a distilled spirit made from the blue agave plant, primarily found in the region surrounding the city of Tequila, Mexico. Known for its distinctive flavor, it’s the key ingredient in Margaritas and Tequila Sunrises.',
+  },
+  {
+    name: 'Triple Sec',
+    description:
+      'Triple Sec is an orange-flavored liqueur used in a variety of cocktails. Its citrusy sweetness is pivotal in drinks like the Cosmopolitan and Sidecar.',
+  },
+  {
+    name: 'Vermouth',
+    description:
+      'Vermouth is a fortified wine flavored with aromatic herbs and spices. There are two primary types: sweet (often red) and dry (usually white). It is a vital ingredient in classic cocktails like the Martini and Manhattan.',
+  },
+  {
+    name: 'Vodka',
+    description:
+      'Vodka is a clear distilled alcoholic beverage with origins in Russia and Poland. It is known for its neutral character and versatility, making it a popular choice for a wide range of cocktails from the Bloody Mary to the Moscow Mule.',
+  },
+  {
+    name: 'Whiskey',
+    description:
+      'Whiskey is a distilled alcoholic beverage made from fermented grain mash. Depending on the region and style, it can be made from barley, corn, rye, and wheat. It forms the foundation for many beloved cocktails like the Old Fashioned and Whiskey Smash.',
+  },
+  {
+    name: 'White Crème de Menthe',
+    description:
+      'White Crème de Menthe is a sweet, mint-flavored liqueur. Its intense minty flavor and clear color make it a favorite for cocktails like the Grasshopper and Stinger.',
+  },
+  {
+    name: 'Absinthe',
+    description:
+      'Absinthe is a highly alcoholic spirit derived from botanicals, including the flowers and leaves of Artemisia absinthium, or wormwood. It has a characteristic anise flavor and played a central role in the cultural scene of 19th-century Paris. It’s traditionally diluted with water poured over a sugar cube.',
+  },
+  {
+    name: 'Angostura Bitters',
+    description:
+      'Angostura Bitters is a concentrated bitters made of water, alcohol, herbs, and spices. Originally from Venezuela, it’s now a staple in many cocktails, providing a complex layer of flavor and aromatic essence.',
+  },
+  {
+    name: 'Aperol',
+    description:
+      'Aperol is an Italian aperitif that is bright orange in color and has a unique taste derived from sweet and bitter oranges, herbs, and roots. It’s a key ingredient in the popular Aperol Spritz cocktail.',
+  },
+  {
+    name: 'Bacardi',
+    description:
+      'Bacardi is a renowned brand of rum, founded in Cuba in 1862. Its portfolio ranges from light to dark rums and is used in numerous cocktails, with the Bacardi Cocktail being one of its signatures.',
+  },
+  {
+    name: 'Baileys Irish Cream',
+    description:
+      'Baileys Irish Cream is a cream-based liqueur, made by blending Irish whiskey with cream, sugar, and cocoa. Its rich and creamy texture makes it a favorite in coffee, desserts, and certain cocktails.',
+  },
+  {
+    name: 'Blue Curaçao',
+    description:
+      'Blue Curaçao is a bright blue liqueur flavored with the dried peel of the Laraha citrus fruit. Its vibrant color and orange flavor make it a popular choice for tropical and visually striking cocktails.',
+  },
+  {
+    name: 'Cherry Brandy',
+    description:
+      'Cherry Brandy is a liqueur infused with cherries and brandy. Its rich cherry flavor and deep red hue make it a delightful addition to various cocktails, especially those that benefit from a fruity touch.',
+  },
+  {
+    name: 'Coconut Cream',
+    description:
+      'Coconut Cream is a thick and creamy liquid extracted from grated coconut. It’s an essential ingredient in tropical cocktails such as the Piña Colada, adding a rich, velvety texture and authentic coconut flavor.',
+  },
+  {
+    name: 'Crème de Cassis',
+    description:
+      'Crème de Cassis is a sweet liqueur made from black currants. Originating in France, it’s the primary ingredient in cocktails like the Kir and Kir Royale, where it lends a fruity depth and a lovely deep red color.',
+  },
+  {
+    name: 'Crème de Cacao',
+    description:
+      'Crème de Cacao is a sweet liqueur that comes in both dark and white varieties, flavored primarily with cacao beans. It adds a chocolatey dimension to cocktails and is often used in dessert drinks like the Chocolate Martini.',
+  },
+  {
+    name: 'Elderflower Liqueur',
+    description:
+      'Elderflower Liqueur is a fragrant and sweet liqueur infused with the aroma and flavor of elderflowers. It’s become increasingly popular in modern mixology, known for adding a floral note to cocktails like the Elderflower Collins.',
+  },
+  {
+    name: 'Galliano',
+    description:
+      'Galliano is a sweet herbal liqueur with a distinct yellow color, known for its complex flavor profile that includes anise, juniper, musk yarrow, and lavender. It’s a key ingredient in the Harvey Wallbanger cocktail.',
+  },
+  {
+    name: 'Ginger Beer',
+    description:
+      "Ginger Beer is a non-alcoholic beverage that’s spicy, sweet, and carbonated. Made from fermented ginger, it’s commonly used in cocktails like the Moscow Mule and Dark 'n' Stormy for its fiery kick and effervescence.",
+  },
+  {
+    name: 'Grand Marnier',
+    description:
+      'Grand Marnier is a French brand of liqueur made from a blend of Cognac brandy, distilled essence of bitter orange, and sugar. Its rich and smooth orange flavor makes it a favorite in desserts and cocktails like the Margarita.',
+  },
+  {
+    name: 'Grapefruit Juice',
+    description:
+      'Grapefruit Juice is a tart and slightly sweet citrus juice, offering a unique flavor profile to a variety of cocktails. Drinks like the Paloma and Sea Breeze are enhanced by its zesty and refreshing qualities.',
+  },
+  {
+    name: 'Green Chartreuse',
+    description:
+      'Green Chartreuse is a French liqueur made by Carthusian monks since the 1740s. It’s composed of distilled alcohol flavored with 130 different plants. The liqueur is known for its distinct vibrant green color and unique herbal flavor.',
+  },
+  {
+    name: 'Honey Syrup',
+    description:
+      'Honey Syrup is a sweetener made from a combination of honey and water, often used to bring a rich, natural sweetness to cocktails. Its unique flavor profile pairs well with whiskey and rum-based drinks.',
+  },
+  {
+    name: 'Lillet Blanc',
+    description:
+      'Lillet Blanc is a French aperitif wine made from Bordeaux grapes combined with macerated fruit liqueurs. It has a slightly sweet, fruity, and herbal character, making it a prime ingredient in cocktails like the Vesper Martini.',
+  },
+  {
+    name: 'Lychee Liqueur',
+    description:
+      'Lychee Liqueur captures the exotic and fragrant essence of the lychee fruit. With its sweet and floral notes, it adds an oriental twist to cocktails and is often paired with vodka or gin.',
+  },
+  {
+    name: 'Muddled Fruit',
+    description:
+      'Muddling is a technique used to release the flavors of fresh fruits, herbs, or spices in cocktails. By pressing ingredients against the side of a glass with a muddler, juices are extracted and flavors intensified, making it essential for drinks like the Mojito or Old Fashioned.',
+  },
+  {
+    name: 'Orange Bitters',
+    description:
+      'Orange Bitters is a type of bitters made from the peels of Seville oranges, cardamom, caraway seed, coriander, and burnt sugar. It’s a flavor-enhancing agent that adds a citrusy complexity to cocktails, especially gin and whiskey creations.',
+  },
+  {
+    name: 'Pernod',
+    description:
+      'Pernod is an anise-flavored spirit, similar to absinthe but without the wormwood. With its strong licorice flavor, it’s typically diluted with water and is a staple in classic cocktails such as the Sazerac.',
+  },
+  {
+    name: 'Pisco',
+    description:
+      'Pisco is a colorless grape brandy popular in Chile and Peru. It’s known for its strong, aromatic character and serves as the base spirit for the iconic Pisco Sour cocktail.',
+  },
+  {
+    name: 'Raspberry Liqueur',
+    description:
+      'Raspberry Liqueur is a sweet liqueur infused with the rich flavor of raspberries. It adds a fruity, vibrant touch to cocktails and pairs well with champagne in drinks like the Kir Royale.',
+  },
+  {
+    name: 'Red Wine',
+    description:
+      "Red Wine isn't just for sipping – it’s also an ingredient in some cocktails. While its rich flavors vary based on the type and region, it can provide depth and nuance to drinks like the Sangria or New York Sour.",
+  },
 ]
