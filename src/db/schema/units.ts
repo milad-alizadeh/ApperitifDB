3e1f1473ac93f0a66b28ef5ad13581612a175170
Milad Alizadeh <milaadd@gmail.com>
2023-09-07 22:00:39 +0100

    add unit types as enum

--- src/db/schema/units.ts
+++ src/db/schema/units.ts
@@ -1,9 +1,29 @@
-import { uuid, text, pgTable } from 'drizzle-orm/pg-core'
+import { uuid, pgTable, pgEnum, boolean } from 'drizzle-orm/pg-core'
 import { createdAt, updatedAt } from '../helpers'
+
+const nameEnum = pgEnum('name', [
+  'oz',
+  'ml',
+  'dash',
+  'drop',
+  'tsp',
+  'tbsp',
+  'cup',
+  'pint',
+  'shot',
+  'grams',
+  'twist',
+  'wheel',
+  'wedge',
+  'pinch',
+  'part',
+  'rim',
+])
 
 export const units = pgTable('units', {
   id: uuid('id').defaultRandom().primaryKey(),
-  name: text('name'),
+  name: nameEnum('name').default('ml'),
+  isConvertable: boolean('is_convertable').default(false),
   createdAt,
   updatedAt,
 })
