fb35aab64970cd67f7551cc9508d8c5945581948
Milad Alizadeh <milaadd@gmail.com>
2023-09-07 19:48:17 +0100

    add categories seed data

--- src/seedData/categories.ts
+++ src/seedData/categories.ts
@@ -1,27 +1,124 @@
 export default [
-  'Refreshing',
-  'Classic',
-  'Tropical',
-  'Fiery',
-  'Strong',
-  'Bold',
-  'Sophisticated',
-  'Luxurious',
-  'Elegant',
-  'Bubbly',
-  'Full-bodied',
-  'Crisp',
-  'Fruity',
-  'Bitter',
-  'Aromatic',
-  'Savory',
-  'Citrusy',
-  'Orangey',
-  'Complex',
-  'Blue',
-  'Fuzzy',
-  'Melon',
-  'Creamy',
-  'Coffee',
-  'Nutty',
+  {
+    name: 'Romantic',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Romantic',
+  },
+  {
+    name: 'Dinner Party',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Dinner+Party',
+  },
+  {
+    name: 'Celebration',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Celebration',
+  },
+  {
+    name: 'Relaxing Evening',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Relax',
+  },
+  {
+    name: 'Summer Chill',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Summer',
+  },
+  {
+    name: 'Winter Warmers',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Winter+Warmers',
+  },
+  {
+    name: 'Tropical Vacation',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Tropical',
+  },
+  {
+    name: 'Girls Night Out',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Girls+Night',
+  },
+  {
+    name: 'Adventure',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Adventure',
+  },
+  {
+    name: 'Retro Vibes',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Retro',
+  },
+
+  {
+    name: 'Vodka-Based',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Vodka',
+  },
+  {
+    name: 'Whiskey-Based',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Whiskey',
+  },
+  {
+    name: 'Rum-Based',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Rum',
+  },
+  {
+    name: 'Tequila-Based',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Tequila',
+  },
+  {
+    name: 'Gin-Based',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Gin',
+  },
+  {
+    name: 'Brandy-Based',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Brandy',
+  },
+  {
+    name: 'Champagne-Based',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Champagne',
+  },
+  {
+    name: 'Wine-Based',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Wine',
+  },
+  {
+    name: 'Beer-Based',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Beer',
+  },
+  {
+    name: 'Cognac-Based',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Cognac',
+  },
+
+  {
+    name: 'Fruity',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Fruit',
+  },
+  {
+    name: 'Spicy',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Spicy',
+  },
+  {
+    name: 'Herbal',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Herbs',
+  },
+  {
+    name: 'Sweet',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Sweet',
+  },
+  {
+    name: 'Bitter',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Bitter',
+  },
+  {
+    name: 'Sour',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Sour',
+  },
+  {
+    name: 'Chocolatey',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Chocolate',
+  },
+  {
+    name: 'Coffee-flavored',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Coffee',
+  },
+  {
+    name: 'Creamy',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Cream',
+  },
+  {
+    name: 'Nutty',
+    imageUrl: 'https://source.unsplash.com/random/500x500/?Nuts',
+  },
 ]
