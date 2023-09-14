2c0b49803d0d0a3c35eb48f07c93cff203f8facc
Milad Alizadeh <milaadd@gmail.com>
2023-09-14 10:24:59 +0100

    update recipe function to include search_term

--- supabase/postgres/functions/get_recipes_by_category_id.sql
+++ supabase/postgres/functions/get_recipes_by_category_id.sql
@@ -1,3 +1,8 @@
+-- Set the similarity threshold for pg_trgm.
+SET
+  pg_trgm.similarity_threshold = 0.5;
+
+
 -- Drop the function if it already exists
 drop function if exists get_recipes_by_category_ids (uuid[], int, int);
 
@@ -22,8 +27,8 @@ create type recipes_page_info as (
 );
 
 
--- Define a function to fetch recipes based on given category IDs and pagination parameters.
 create function get_recipes_by_category_ids (
+  search_term text,
   category_ids uuid[],
   page_number int,
   page_size int
@@ -34,15 +39,20 @@ declare
     page_info recipes_page_info;
     -- Flag to check if category_ids is empty.
     is_empty_categories boolean := coalesce(array_length(category_ids, 1), 0) = 0;
+    -- Flag to check if search_term is empty.
+    is_empty_search_term boolean := search_term IS NULL OR search_term = '';
 begin
     -- Using a CTE (Common Table Expression) for ordered recipes.
-    -- This fetches distinct recipes (based on given category IDs) in a paginated manner.
+    -- This fetches distinct recipes (based on given category IDs and search_term) in a paginated manner.
     with ordered_recipes as (
         select distinct r.id, r.name, r.image_url
         from recipes r
         left join recipes_categories rc on r.id = rc.recipe_id
-        where is_empty_categories or rc.category_id = any(category_ids)
-        order by r.name asc
+        where (is_empty_categories or rc.category_id = any(category_ids))
+        and (is_empty_search_term or r.name % search_term)
+        order by         
+          case when is_empty_search_term then null else similarity(r.name, search_term) end desc,
+          r.name asc
         limit page_size
         offset (page_number - 1) * page_size
     )
@@ -56,7 +66,8 @@ begin
     into total_count
     from recipes r
     left join recipes_categories rc on r.id = rc.recipe_id
-    where is_empty_categories or rc.category_id = any(category_ids);
+    where (is_empty_categories or rc.category_id = any(category_ids))
+    and (is_empty_search_term or r.name % search_term);
 
     -- Assign values to the custom return type: recipes_page_info.
     page_info := row(
