05eaae9c83eaa937a6ed1b1e3b991fe314bfc424
Milad Alizadeh <milaadd@gmail.com>
2023-09-14 15:23:48 +0100

    add ingredient search

--- supabase/postgres/functions/get_recipes_by_category_id.sql
+++ supabase/postgres/functions/get_recipes_by_category_id.sql
@@ -1,82 +1,104 @@
--- Set the similarity threshold for pg_trgm.
-SET
-  pg_trgm.similarity_threshold = 0.5;
+-- set the similarity threshold for pg_trgm.
+set
+    pg_trgm.similarity_threshold = 0.2;
 
+-- drop the function if it already exists
+drop function if exists get_recipes_by_category_ids (text, uuid[], int, int) cascade;
 
--- Drop the function if it already exists
-drop function if exists get_recipes_by_category_ids (uuid[], int, int);
+-- drop the custom types if they already exist.
+drop type if exists recipes_page_info cascade;
 
+drop type if exists recipe_preview cascade;
 
--- Drop the custom types if they already exist.
-drop type if exists recipes_page_info;
-
-
--- Drop the recipe_preview type if it already exists.
-drop type if exists recipe_preview;
-
-
--- Define a custom type to represent a recipe preview.
+-- define a custom type to represent a recipe preview.
 create type recipe_preview as (id uuid, name text, image_url text);
 
-
--- Define a custom type for the function's return value
+-- define a custom type for the function's return value
 create type recipes_page_info as (
-  recipes recipe_preview[],
-  total_count int,
-  has_next_page boolean
+    recipes recipe_preview[],
+    total_count int,
+    has_next_page boolean
 );
 
-
 create function get_recipes_by_category_ids (
-  search_term text,
-  category_ids uuid[],
-  page_number int,
-  page_size int
+    search_term text,
+    category_ids uuid[],
+    page_number int,
+    page_size int
 ) returns recipes_page_info as $$
 declare
     total_count int;
     recipes_result recipe_preview[];
     page_info recipes_page_info;
-    -- Flag to check if category_ids is empty.
     is_empty_categories boolean := coalesce(array_length(category_ids, 1), 0) = 0;
-    -- Flag to check if search_term is empty.
-    is_empty_search_term boolean := search_term IS NULL OR search_term = '';
+    is_empty_search_term boolean := search_term is null or search_term = '';
 begin
-    -- Using a CTE (Common Table Expression) for ordered recipes.
-    -- This fetches distinct recipes (based on given category IDs and search_term) in a paginated manner.
-    with ordered_recipes as (
-        select distinct r.id, r.name, r.image_url
+    -- compute similarity scores for recipes based on search term.
+    with similarity_scores as (
+        select 
+            r.id, 
+            r.name, 
+            r.image_url, 
+            max(case when is_empty_search_term then null 
+                 else similarity(r.name, search_term) 
+            end) as recipe_sim_score,
+            max(case when is_empty_search_term then null 
+                 else similarity(i.name, search_term) 
+            end) as ingredient_sim_score
         from recipes r
         left join recipes_categories rc on r.id = rc.recipe_id
-        where (is_empty_categories or rc.category_id = any(category_ids))
-        and (is_empty_search_term or r.name % search_term)
+        left join recipes_ingredients ri on r.id = ri.recipe_id
+        left join ingredients i on ri.ingredient_id = i.id
+        where 
+            (is_empty_categories or rc.category_id = any(category_ids))
+            and (
+                is_empty_search_term 
+                or r.name % search_term
+                or i.name % search_term
+            )
+        group by r.id, r.name, r.image_url
+    ),
+    ordered_recipes as (
+        select 
+            id, 
+            name, 
+            image_url
+        from similarity_scores
         order by         
-          case when is_empty_search_term then null else similarity(r.name, search_term) end desc,
-          r.name asc
+            recipe_sim_score desc,
+            ingredient_sim_score desc,
+            name asc
         limit page_size
         offset (page_number - 1) * page_size
     )
-    -- Populate the recipes_result from the CTE.
+    -- populate the recipes_result from the cte.
     select array_agg(row(id, name, image_url)::recipe_preview) 
     into recipes_result
     from ordered_recipes;
 
-    -- Compute the total count of matching recipes.
+    -- compute the total count of matching recipes.
     select count(distinct r.id)
     into total_count
     from recipes r
     left join recipes_categories rc on r.id = rc.recipe_id
-    where (is_empty_categories or rc.category_id = any(category_ids))
-    and (is_empty_search_term or r.name % search_term);
+    left join recipes_ingredients ri on r.id = ri.recipe_id
+    left join ingredients i on ri.ingredient_id = i.id
+    where 
+        (is_empty_categories or rc.category_id = any(category_ids))
+        and (
+            is_empty_search_term 
+            or r.name % search_term
+            or i.name % search_term
+        );
 
-    -- Assign values to the custom return type: recipes_page_info.
+    -- assign values to the custom return type: recipes_page_info.
     page_info := row(
         recipes_result, 
         total_count, 
         total_count > page_size * page_number
     )::recipes_page_info;
 
-    -- Return the computed page info.
+    -- return the computed page info.
     return page_info;
 end;
 $$ language plpgsql;
