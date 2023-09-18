8f3219b739ffc2c2275dafd99c53c87776af9d20
Milad Alizadeh <milaadd@gmail.com>
2023-09-18 21:06:23 +0100

    add create trigger function

---
+++ supabase/postgres/functions/create_trigger_for_table.sql
@@ -0,0 +1,11 @@
+create
+or replace function create_trigger_for_table (table_name text) returns void language plpgsql as $$
+begin
+    execute format('
+        create trigger handle_updated_at 
+        before update on %i 
+        for each row 
+        execute procedure moddatetime (updated_at);
+    ', table_name);
+end;
+$$;
---
+++ supabase/postgres/scripts/add_automatic_timestamp.sql
@@ -0,0 +1,32 @@
+SELECT
+  create_trigger_for_table ('categories');
+
+SELECT
+  create_trigger_for_table ('content_apperitivo');
+
+SELECT
+  create_trigger_for_table ('equipments');
+
+SELECT
+  create_trigger_for_table ('ingredients_categories');
+
+SELECT
+  create_trigger_for_table ('ingredients');
+
+SELECT
+  create_trigger_for_table ('recipes_categories');
+
+SELECT
+  create_trigger_for_table ('recipes_equipments');
+
+SELECT
+  create_trigger_for_table ('recipes_ingredients');
+
+SELECT
+  create_trigger_for_table ('recipes');
+
+SELECT
+  create_trigger_for_table ('steps');
+
+SELECT
+  create_trigger_for_table ('units');
