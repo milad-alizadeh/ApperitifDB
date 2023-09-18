52096ec6b92244f982c99e726f9f2cb234489437
Milad Alizadeh <milaadd@gmail.com>
2023-09-18 21:06:42 +0100

    fix typo

--- supabase/postgres/functions/create_trigger_for_table.sql
+++ supabase/postgres/functions/create_trigger_for_table.sql
@@ -3,7 +3,7 @@ or replace function create_trigger_for_table (table_name text) returns void lang
 begin
     execute format('
         create trigger handle_updated_at 
-        before update on %i 
+        before update on %I 
         for each row 
         execute procedure moddatetime (updated_at);
     ', table_name);
