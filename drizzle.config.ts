0b934f0d84a8bbb87cbb8389d7e80dc87b741a8c
Milad Alizadeh <milaadd@gmail.com>
2023-09-22 16:05:53 +0100

    add drizzle kit connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,15 @@
 import type { Config } from 'drizzle-kit'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+console.log(process.env.DB_URL, 'url')
 
 export default {
   schema: './src/db/schema',
   out: './supabase/migrations',
   driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.DB_URL,
+  },
 } satisfies Config
