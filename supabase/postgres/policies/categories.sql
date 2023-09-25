839b6d7eb8f1f108f824199fbfb01768b0c56ae0
Milad Alizadeh <milaadd@gmail.com>
2023-09-26 00:47:08 +0100

    update categories policy

--- supabase/postgres/policies/categories.sql
+++ supabase/postgres/policies/categories.sql
@@ -1,3 +1,5 @@
+alter table categories enable row level security;
+
 drop policy if exists "Categories are viewable by everyone." on categories;
 
 create policy "Categories are viewable by everyone." on categories for
