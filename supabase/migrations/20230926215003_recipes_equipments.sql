65e1a98274cc504f13a8a6900082241202a8e720
Milad Alizadeh <milaadd@gmail.com>
2023-09-26 22:11:52 +0100

    update recipes equipments

--- supabase/migrations/20230926215003_recipes_equipments.sql
+++ supabase/migrations/20230926215003_recipes_equipments.sql
@@ -1,8 +1,8 @@
-alter table equipments enable row level security;
+alter table recipes_equipments enable row level security;
 
-drop policy if exists "Equipments are viewable by everyone." on equipments;
+drop policy if exists "Recipes Equipments are viewable by everyone." on recipes_equipments;
 
-create policy "Equipments are viewable by everyone." on equipments for
+create policy "Recipes Equipments are viewable by everyone." on recipes_equipments for
 select
   to authenticated,
   anon using (true);
