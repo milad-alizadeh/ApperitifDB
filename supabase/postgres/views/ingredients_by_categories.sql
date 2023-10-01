4f7b2b5184a97089e26ce4000774ad4a6583745d
Milad Alizadeh <milaadd@gmail.com>
2023-10-01 14:21:13 +0100

    add new view

---
+++ supabase/postgres/views/ingredients_by_categories.sql
@@ -0,0 +1,17 @@
+drop view if exists ingredients_by_categories;
+
+create view
+  ingredients_by_categories as
+select
+  c.id as id,
+  c.name as title,
+  json_agg(json_build_object('id', i.id, 'name', i.name)) as data,
+  count(i.id) as count
+from
+  categories c
+  join ingredients_categories ic on c.id = ic.category_id
+  join ingredients i on i.id = ic.ingredient_id
+group by
+  c.id
+order by
+  c.name;
