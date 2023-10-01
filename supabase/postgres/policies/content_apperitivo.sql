30fba47eb64fd473aac6827655e418888752f5fc
Milad Alizadeh <milaadd@gmail.com>
2023-10-02 00:27:34 +0100

    remove old policy

--- supabase/postgres/policies/content_apperitivo.sql
+++
@@ -1,8 +0,0 @@
-alter table content_apperitivo enable row level security;
-
-drop policy if exists "Contents are viewable by everyone." on content_apperitivo;
-
-create policy "Contents are viewable by everyone." on content_apperitivo for
-select
-  to authenticated,
-  anon using (true);
