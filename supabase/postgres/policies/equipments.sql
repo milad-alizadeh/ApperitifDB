5e2761adc86e6dac11892df6407c108e303e86c9
Milad Alizadeh <milaadd@gmail.com>
2023-10-02 18:13:58 +0100

    delete old policies

--- supabase/postgres/policies/equipments.sql
+++
@@ -1,8 +0,0 @@
-alter table equipments enable row level security;
-
-drop policy if exists "Equipments are viewable by everyone." on equipments;
-
-create policy "Equipments are viewable by everyone." on equipments for
-select
-  to authenticated,
-  anon using (true);
