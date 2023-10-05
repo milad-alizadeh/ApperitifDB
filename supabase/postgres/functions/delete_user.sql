6da44d0e3704eb820cbef81acc0dd62915886c18
Milad Alizadeh <milaadd@gmail.com>
2023-10-05 23:56:26 +0100

    update delete user

--- supabase/postgres/functions/delete_user.sql
+++ supabase/postgres/functions/delete_user.sql
@@ -1,4 +1,4 @@
 create
-or replace function "deleteUser" () returns void language sql security definer as $$
+or replace function "delete_user" () returns void language sql security definer as $$
    delete from auth.users where id = auth.uid();
 $$;
