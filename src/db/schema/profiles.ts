b1e1aab4ee61b17eac20fb9a1d33790d5b410704
Milad <milaadd@gmail.com>
2023-11-16 18:14:52 +0000

    add role column to users

--- src/db/schema/profiles.ts
+++ src/db/schema/profiles.ts
@@ -1,8 +1,9 @@
-import { uuid, pgTable } from 'drizzle-orm/pg-core'
+import { uuid, pgTable, text } from 'drizzle-orm/pg-core'
 import { createdAt, updatedAt } from '../helpers'
 
 export const profiles = pgTable('profiles', {
   id: uuid('id').notNull().primaryKey(),
+  role: text('role').notNull().default('user'),
   createdAt,
   updatedAt,
 })
