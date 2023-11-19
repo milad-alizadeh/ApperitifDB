29f0ccb5a0db4fe08637f167b375bd034fc6f801
Milad <milaadd@gmail.com>
2023-11-19 18:53:13 +0200

    add total count to gql equipment table

--- supabase/postgres/settings/total_count.sql
+++ supabase/postgres/settings/total_count.sql
@@ -6,3 +6,5 @@ comment on table "ingredients" is e'@graphql({"totalCount": {"enabled": true}})'
 comment on table "recipes" is e'@graphql({"totalCount": {"enabled": true}})';
 
 comment on table "categories" is e'@graphql({"totalCount": {"enabled": true}})';
+
+comment on table "equipment" is e'@graphql({"totalCount": {"enabled": true}})';
