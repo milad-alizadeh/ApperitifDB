85fd02cfe6d9c1083d66b8fc1fbd11dac8022c6e
Milad <milaadd@gmail.com>
2023-11-21 22:53:58 +0000

    add image upload edge function

--- supabase/functions/upload-image/index.ts
+++ supabase/functions/upload-image/index.ts
@@ -1,22 +1,38 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+import { corsHeaders } from '../_shared/cors.ts'
 
-console.log('Hello from Functions!')
+console.log(`Function "browser-with-cors" up and running!`)
 
-Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
+Deno.serve(async (req: any) => {
+  // This is needed if you're planning to invoke your function from a browser.
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
   }
 
-  return new Response(JSON.stringify(data), {
-    headers: { 'Content-Type': 'application/json' },
-  })
-})
+  const supabaseClient = createClient(
+    Deno.env.get('SUPABASE_URL') as string,
+    Deno.env.get('SUPABASE_ANON_KEY') as string,
+  )
 
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
+  const image = new Uint8Array(await req.arrayBuffer())
+  const fileName = Math.random().toString(36).substring(2, 15)
+
+  const { data, error } = await supabaseClient.storage
+    .from('public-images')
+    .upload(`${fileName}.jpg`, image, {
+      contentType: 'image/jpeg',
+      upsert: true,
+    })
+
+  if (error) {
+    return new Response(JSON.stringify(error.message), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: error.statusCode,
+    })
+  } else {
+    return new Response(JSON.stringify(data), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 200,
+    })
+  }
+})
