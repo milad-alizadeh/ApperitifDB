5e3e7caa9987bb4b369e7333cadf98cd1fe6aa7d
Milad <milaadd@gmail.com>
2023-11-22 18:23:27 +0000

    add admin edit policy to recipes

--- supabase/postgres/policies/recipes.sql
+++ supabase/postgres/policies/recipes.sql
@@ -6,3 +6,29 @@ create policy "Recipes are viewable by everyone." on recipes for
 select
   to authenticated,
   anon using (true);
+
+drop policy if exists "Recipes are editable by admin users." on recipes;
+
+create policy "Recipes are editable by admin users" on recipes for all to authenticated using (
+  exists (
+    select
+      1
+    from
+      profiles
+    where
+      profiles.user_id = auth.uid ()
+      and profiles.role = 'admin'
+  )
+)
+with
+  check (
+    exists (
+      select
+        1
+      from
+        profiles
+      where
+        profiles.user_id = auth.uid ()
+        and profiles.role = 'admin'
+    )
+  );
