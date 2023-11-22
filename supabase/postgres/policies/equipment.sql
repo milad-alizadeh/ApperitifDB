83974fae54558c2bdd50f6f210ec20d982492a03
Milad <milaadd@gmail.com>
2023-11-22 19:08:24 +0000

    add admin policy to equipment

--- supabase/postgres/policies/equipment.sql
+++ supabase/postgres/policies/equipment.sql
@@ -6,3 +6,29 @@ create policy "Equipment are viewable by everyone." on equipment for
 select
   to authenticated,
   anon using (true);
+
+drop policy if exists "Equipment are editable by admin users." on equipment;
+
+create policy "Equipment are editable by admin users." on equipment for all to authenticated using (
+  exists (
+    select
+      1
+    from
+      profiles
+    where
+      profiles.id = auth.uid ()
+      and profiles.role = 'admin'
+  )
+)
+with
+  check (
+    exists (
+      select
+        1
+      from
+        profiles
+      where
+        profiles.id = auth.uid ()
+        and profiles.role = 'admin'
+    )
+  );
