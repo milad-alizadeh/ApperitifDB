8c29be0eb7f6f14d0ecb2f80ce439a8414ab63b4
Milad <milaadd@gmail.com>
2023-11-22 19:57:31 +0000

    add admin policy to steps

--- supabase/postgres/policies/steps.sql
+++ supabase/postgres/policies/steps.sql
@@ -6,3 +6,29 @@ create policy "Steps are viewable by everyone." on steps for
 select
   to authenticated,
   anon using (true);
+
+drop policy if exists "Steps are editable by admin users." on steps;
+
+create policy "Steps are editable by admin users." on steps for all to authenticated using (
+  exists (
+    select
+      1
+    from
+      profiles
+    where
+      profiles.id = auth.uid ()
+      and profiles.role = 'admin'
+  )
+)
+with
+  check (
+    exists (
+      select
+        1
+      from
+        profiles
+      where
+        profiles.id = auth.uid ()
+        and profiles.role = 'admin'
+    )
+  );
