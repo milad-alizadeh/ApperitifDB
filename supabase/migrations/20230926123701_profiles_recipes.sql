aec11bbed18a9fc4a2caa495cecce735bd7ea3d6
Milad <milaadd@gmail.com>
2023-11-22 20:37:27 +0000

    update migrations

--- supabase/migrations/20230926123701_profiles_recipes.sql
+++ supabase/migrations/20230926123701_profiles_recipes.sql
@@ -1,6 +1,6 @@
 alter table profiles_recipes enable row level security;
 
-drop policy if exists "Users can access their own favourite recipes." on recipes;
+drop policy if exists "Users can access their own favourite recipes." on profiles_recipes;
 
 create policy "Users can access their own favourite recipes." on profiles_recipes for all to authenticated using (auth.uid () = profile_id)
 with
--- supabase/migrations/20230926220407_profiles_ingredients.sql
+++ supabase/migrations/20230926220407_profiles_ingredients.sql
@@ -1,5 +1,7 @@
 alter table profiles_ingredients enable row level security;
 
+drop policy if exists "Users can access ingredients in their bar." on profiles_ingredients;
+
 create policy "Users can access ingredients in their bar." on profiles_ingredients for all to authenticated using (auth.uid () = profile_id)
 with
   check (auth.uid () = profile_id);
