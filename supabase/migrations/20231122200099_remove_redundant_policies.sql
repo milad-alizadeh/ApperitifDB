000c2dd2b3a345fc28932264879b829284b0f09b
Milad <milaadd@gmail.com>
2023-11-22 21:09:11 +0000

    remove redundant migrations

---
+++ supabase/migrations/20231122200099_remove_redundant_policies.sql
@@ -0,0 +1,9 @@
+drop policy if exists "Users can access and edit their own profiles." on profiles;
+
+drop policy if exists "Users can access their own favourite recipes." on profiles;
+
+drop policy if exists "Recipes Equipments are viewable by everyone." on recipes_equipment;
+
+drop policy if exists "Equipment are viewable by everyone." on equipment;
+
+drop policy if exists "Equipments are viewable by everyone." on equipment;
