55ec75946fdd17a956d04ca63e7ce4e3771412bf
Milad <milaadd@gmail.com>
2023-11-22 19:08:03 +0000

    add admin policy to categories

--- supabase/postgres/policies/categories.sql
+++ supabase/postgres/policies/categories.sql
@@ -6,3 +6,29 @@ create policy "Categories are viewable by everyone." on categories for
 select
   to authenticated,
   anon using (true);
+
+drop policy if exists "Categories are editable by admin users." on categories;
+
+create policy "Categories are editable by admin users." on categories for all to authenticated using (
+  exists (
+    select
+      1
+    from
+      profiles
+    where
+      profiles.id = auth.uid ()
+      and profiles.role = 'admin'
+  )
+)
+with
+  check (
+    exists (
+      select
+        1
+      from
+        profiles
+      where
+        profiles.id = auth.uid ()
+        and profiles.role = 'admin'
+    )
+  );
