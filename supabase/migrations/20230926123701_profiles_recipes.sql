7fa50049f92b80a9f85ee627e8e38ad8d56d47c1
Milad <milaadd@gmail.com>
2023-11-22 20:36:02 +0000

    update policy

--- supabase/migrations/20230926123701_profiles_recipes.sql
+++ supabase/migrations/20230926123701_profiles_recipes.sql
@@ -1,5 +1,7 @@
 alter table profiles_recipes enable row level security;
 
+drop policy if exists "Users can access their own favourite recipes." on recipes;
+
 create policy "Users can access their own favourite recipes." on profiles_recipes for all to authenticated using (auth.uid () = profile_id)
 with
   check (auth.uid () = profile_id);
