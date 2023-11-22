8e401a910442ca2edab8e4b76e8c9ef4865627c9
Milad <milaadd@gmail.com>
2023-11-22 20:00:31 +0000

    fix typo

--- supabase/postgres/policies/equipment.sql
+++ supabase/postgres/policies/equipment.sql
@@ -1,15 +1,15 @@
 alter table equipment enable row level security;
 
-drop policy if exists "Equipment are viewable by everyone." on equipment;
+drop policy if exists "Equipment is viewable by everyone." on equipment;
 
-create policy "Equipment are viewable by everyone." on equipment for
+create policy "Equipment is viewable by everyone." on equipment for
 select
   to authenticated,
   anon using (true);
 
-drop policy if exists "Equipment are editable by admin users." on equipment;
+drop policy if exists "Equipment is editable by admin users." on equipment;
 
-create policy "Equipment are editable by admin users." on equipment for all to authenticated using (
+create policy "Equipment is editable by admin users." on equipment for all to authenticated using (
   exists (
     select
       1
