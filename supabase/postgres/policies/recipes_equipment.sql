efcd691467a6f8877b582dfd33e15dbdbb1ba075
Milad <milaadd@gmail.com>
2023-11-22 19:49:46 +0000

    add admin policy to recipes_equipment

--- supabase/postgres/policies/recipes_equipment.sql
+++ supabase/postgres/policies/recipes_equipment.sql
@@ -1,8 +1,34 @@
-alter table equipment enable row level security;
+alter table recipes_equipment enable row level security;
 
-drop policy if exists "Equipment are viewable by everyone." on equipment;
+drop policy if exists "Recipes Equipment are viewable by everyone." on recipes_equipment;
 
-create policy "Equipment are viewable by everyone." on equipment for
+create policy "Recipes Equipment are viewable by everyone." on recipes_equipment for
 select
   to authenticated,
   anon using (true);
+
+drop policy if exists "Recipes Equipment are editable by admin users." on recipes_equipment;
+
+create policy "Recipes Equipment are editable by admin users." on recipes_equipment for all to authenticated using (
+  exists (
+    select
+      1
+    from
+      profiles
+    where
+      profiles.id = auth.uid ()
+      and profiles.role = 'admin'
+  )
+)
+with
+  check (
+    exists (
+      select
+        1
+      from
+        profiles
+      where
+        profiles.id = auth.uid ()
+        and profiles.role = 'admin'
+    )
+  );
