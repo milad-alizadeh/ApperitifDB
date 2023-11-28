47542c0e8c6ce0db06fcf6f388901377b9ffa2d8
Milad <milaadd@gmail.com>
2023-11-28 00:06:53 +0000

    add new migration

---
+++ supabase/migrations/20231128000642_total_count.sql
@@ -0,0 +1,12 @@
+-- Add total count to ingredients_categories table.
+comment on table "ingredients_categories" is e'@graphql({"totalCount": {"enabled": true}})';
+
+comment on table "ingredients" is e'@graphql({"totalCount": {"enabled": true}})';
+
+comment on table "recipes" is e'@graphql({"totalCount": {"enabled": true}})';
+
+comment on table "categories" is e'@graphql({"totalCount": {"enabled": true}})';
+
+comment on table "equipment" is e'@graphql({"totalCount": {"enabled": true}})';
+
+comment on table "app_content" is e'@graphql({"totalCount": {"enabled": true}})';
