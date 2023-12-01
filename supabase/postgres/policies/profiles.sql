e22f148d3d984abd2799e6bfde4c4391d2cc8f1c
Milad <milaadd@gmail.com>
2023-12-01 23:19:13 +0000

    add profile delete permission to user

--- supabase/postgres/policies/profiles.sql
+++ supabase/postgres/policies/profiles.sql
@@ -2,6 +2,10 @@ alter table profiles enable row level security;
 
 drop policy if exists "Users can view their own profile" on recipes_ingredients;
 
+drop policy if exists "Users can delete their own profile" on recipes_ingredients;
+
 create policy "Users can view their own profile" on profiles for
 select
   to authenticated using (auth.uid () = id);
+
+create policy "Users can delete their own profile" on profiles for delete to authenticated using (auth.uid () = id);
