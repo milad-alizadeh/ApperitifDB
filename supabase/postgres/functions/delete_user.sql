17ec06ba485d58d2f9eba774eb01f25588d603bb
Milad <milaadd@gmail.com>
2023-12-01 23:44:19 +0000

    fix delete user funtion

--- supabase/postgres/functions/delete_user.sql
+++ supabase/postgres/functions/delete_user.sql
@@ -1,11 +1,14 @@
 create
-or replace function delete_user () returns boolean language plpgsql as $$
+or replace function delete_user () returns boolean language plpgsql security definer as $$
+declare
+    row_count int;
 begin
-   delete from auth.users where id = auth.uid();
-   if found then
-       return true;
-   else
-       return false;
-   end if;
+    delete from auth.users where id = auth.uid();
+    get diagnostics row_count = row_count;
+    if row_count > 0 then
+        return true;
+    else
+        return false;
+    end if;
 end;
 $$;
