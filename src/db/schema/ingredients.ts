c969b000ec8c690808538fcf4151067a569fdc0a
Milad <milaadd@gmail.com>
2023-12-08 01:06:44 +0000

    add is_draft field to ingredients

--- src/db/schema/ingredients.ts
+++ src/db/schema/ingredients.ts
@@ -1,10 +1,11 @@
-import { uuid, text, pgTable } from 'drizzle-orm/pg-core'
+import { uuid, text, pgTable, boolean } from 'drizzle-orm/pg-core'
 import { createdAt, updatedAt } from '../helpers'
 
 export const ingredients = pgTable('ingredients', {
   id: uuid('id').defaultRandom().primaryKey(),
   name: text('name').notNull(),
   description: text('description'),
+  isDraft: boolean('is_draft').default(false),
   createdAt,
   updatedAt,
 })
